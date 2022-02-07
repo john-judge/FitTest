acab941606b8d1b61a070d466b5cace4deb0c4a9
john-judge <jjudge@uchicago.edu>
2022-02-07 16:47:41 -0600

    add ability to pull slowtwitch database to offline CSV

--- update.py
+++ update.py
@@ -1,6 +1,11 @@
 import os
 import time
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from datetime import date
 
+'''
 os.system('cmd /c "color a"')
 os.system('cmd /c "cd Desktop"')
 os.system('cmd /c "cd FitTest"')
@@ -8,3 +13,49 @@ os.system('cmd /c "git pull"')
 os.system('cmd /c "conda activate FitTest"')  # needed because Pyinstaller doesn't like to include itself as dependency
 os.system('cmd /c "pyinstaller -n DynamicFitter --add-data source/images/*.png;./source/images/ driver.py --noconfirm"')
 time.sleep(3)
+'''
+
+
+print("\n\n\n ****** \n\n\nNow updating offline bike database from SlowTwitch.")
+
+print("Old version of the database has been archived.")
+
+response = requests.get(
+    'https://docs.google.com/spreadsheets/u/0/d/e/2PACX-1vRu5yNNvCe_v7A_bTWDZvVq-w9VfqmRKIyV8yXBWRrjXEC19hIqOKsZJjgCv_lm-LlUfN7-37WR58wJ/pubhtml/sheet?headers=false&gid=0&output=csv')
+print(response.status_code, type(response.content))
+
+
+path = 'html.html'
+data = []
+
+# for getting the header from
+# the HTML file
+list_header = []
+soup = BeautifulSoup(response.content, 'html.parser')
+header = soup.find_all("table")[0].find("tr")
+
+for items in header:
+    try:
+        list_header.append(items.get_text())
+    except:
+        continue
+
+# for getting the data
+HTML_data = soup.find_all("table")[0].find_all("tr")[1:]
+
+for element in HTML_data:
+    sub_data = []
+    for sub_element in element:
+        try:
+            sub_data.append(sub_element.get_text())
+        except:
+            continue
+    data.append(sub_data)
+
+# Storing the data into Pandas
+# DataFrame
+dataFrame = pd.DataFrame(data=data, columns=list_header)
+
+# Converting Pandas DataFrame
+# into CSV file
+dataFrame.to_csv('slowtwitch_database/DB'+ str(date.today().strftime("%d%m%Y")) + '.csv')
